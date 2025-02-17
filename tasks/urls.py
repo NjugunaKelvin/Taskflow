174ae02983d8e1d6926d8358db8d94378e302387
NjugunaKelvin <njugunak349@gmail.com>
2025-02-17 16:34:23 +0300

    Add task creation and deletion functionality with corresponding views and templates

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.task_list, name='task_list')
+    path('', views.task_list, name='task_list'),
+    path('create/', views.task_create, name='task_create'),
+    path('task/<int:pk>/delete/', views.task_delete, name='task_delete')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Task
 
@@ -6,4 +6,24 @@ from .models import Task
 @login_required
 def task_list(request):
     tasks = Task.objects.filter(user=request.user)
-    return render(request, 'task_list.html', {'tasks' : tasks})
+    return render(request, 'tasks/task_list.html', {'tasks' : tasks})
+
+
+@login_required
+def task_create(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Task.objects.create(user=request.user, title=title, description=description)
+        return redirect('task_list')
+    
+    return render(request, 'tasks/task_form.html')
+    
+
+@login_required
+def task_delete(request, pk):  # Add 'pk' parameter here
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
